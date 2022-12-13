c9940b2cec4cbd3d675e19867488b1cf08714c3d
joshuaqyang <joshuayang2014@gmail.com>
2022-12-13 12:37:26 -0500

    stuff

--- miniassembler.c
+++ miniassembler.c
@@ -66,8 +66,25 @@ unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
 unsigned int MiniAssembler_strb(unsigned int uiFromReg,
    unsigned int uiToReg)
 {
-   /* Your code here */
+   unsigned int uiInstr;
+   unsigned int uiDisp;
 
+   /* Base Instruction Code */
+   uiInstr = 0x39000000;
+
+   /* uiFromReg inserted in instruction */
+   setField(uiFromReg, 0, &uiInstr, 0, 5);
+
+   /* uiToReg inserted in instruction */
+   setField(uiToReg, 0, &uiInstr, 5, 5);
+
+   /* displacement to be split into immlo and immhi and inserted */
+   uiDisp = (unsigned int)(ulAddr - ulAddrOfThisInstr);
+
+   setField(uiDisp, 0, &uiInstr, 29, 2);
+   setField(uiDisp, 2, &uiInstr, 5, 19);
+
+   return uiInstr;
 }
 
 /*--------------------------------------------------------------------*/
