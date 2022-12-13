1a40895495a213f3e514b9c71e494ee5a83e6343
santelli <santelli@princeton.edu>
2022-12-13 12:36:30 -0500

    l

--- miniassembler.c
+++ miniassembler.c
@@ -27,8 +27,15 @@ static void setField(unsigned int uiSrc, unsigned int uiSrcStartBit,
 
 unsigned int MiniAssembler_mov(unsigned int uiReg, int iImmed)
 {
-   /* Your code here */
+   unsigned int uiInstr;
 
+   /* Base Instruction Code */
+   uiInstr = 0xD2800000;
+   
+   setField(uiReg, 0, &uiInstr, 0, 5);
+   setField(iImmed, 0, &uiInstr, 5, 16);
+
+   return uiInstr;
 }
 
 /*--------------------------------------------------------------------*/
@@ -59,25 +66,7 @@ unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
 unsigned int MiniAssembler_strb(unsigned int uiFromReg,
    unsigned int uiToReg)
 {
-   unsigned int uiInstr;
-   unsigned int uiDisp;
-
-   /* Base Instruction Code */
-   uiInstr = 0x39000000;
-
-   /* uiFromReg inserted in instruction */
-   setField(uiFromReg, 0, &uiInstr, 0, 5);
-
-   /* uiToReg inserted in instruction */
-   setField(uiToReg, 0, &uiInstr, 5, 5);
-
-   /* displacement to be split into immlo and immhi and inserted */
-   uiDisp = (unsigned int)(ulAddr - ulAddrOfThisInstr);
-
-   setField(uiDisp, 0, &uiInstr, 29, 2);
-   setField(uiDisp, 2, &uiInstr, 5, 19);
-
-   return uiInstr;
+   /* Your code here */
 
 }
 
